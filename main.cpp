3f561881e1944539e1dbcd2ba660d1e377c93ff2
Vinicius <133075570+Vini-lm@users.noreply.github.com>
2024-04-04 16:35:41 -0300

    novas funções adicionadas...

--- main.cpp
+++ main.cpp
@@ -54,18 +54,53 @@ void Gerador(unsigned int &n)
         cout << "Eh primo" << endl;
 }
 
+bool CheckNumPerfeito(int n)
+{
+
+    int *Divisores = new int[n];
+
+    int Soma = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        Divisores[i] == 0;
+    }
+
+    Divisores[0] = 1;
+    Divisores[n - 1] = n;
+
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            Divisores[i] == i;
+    }
+
+    for (int i = 0; i < n; i++)
+        Soma += Divisores[i];
+
+    if (Soma == n)
+        return true;
+
+    cout << "Soma de " << n << " " << Soma;
+    delete[] Divisores;
+
+    return false;
+}
+
 int main()
 {
     int n2;
     unsigned int n;
+    int n3;
 
-    vPrint(" Ola, este programa gera e testa numeros primos", 30);
-    vPrint("Escolha uma opcao entre 1 e 2", 10);
-    vPrint(" 1 -> Voce digita um numero qualquer e o programa retorna para voce o divisor dele", 10);
-    vPrint("note que se o numero for primo o resultado sera (0)", 10);
-    vPrint(" 2 -> Voce digita um numero qualquer e o programa aplica ele na formula que gera numeros primos", 10);
-    vPrint("e ele verifica se o numero eh primo.", 10);
-    vPrint("Formula (n) = (n * n) + n + 41", 10);
+    vPrint(" Ola, este programa gera e testa numeros primos", 40);
+    vPrint("Escolha uma opcao entre 1 e 2", 1);
+    vPrint(" 1 -> Voce digita um numero qualquer e o programa retorna para voce o divisor dele", 15);
+    vPrint("note que se o numero for primo o resultado sera (0)", 5);
+    vPrint(" 2 -> Voce digita um numero qualquer e o programa aplica ele na formula que gera numeros primos", 5);
+    vPrint("e ele verifica se o numero eh primo.", 5);
+    vPrint("Formula (n) = (n * n) + n + 41", 5);
+    vPrint("Para fechar o programa apenas digite algum numero menor ou igual a 0", 5);
 
     cin >> n2;
 
@@ -85,6 +120,15 @@ int main()
         {
             Gerador(n);
             cin >> n;
+        }
+        break;
+
+    case 3:
+        cin >> n3;
+        while (n3 > 0)
+        {
+            cout << CheckNumPerfeito(n3) << endl;
+            cin >> n3;
         }
         break;
 
